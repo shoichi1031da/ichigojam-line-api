6caca808078aab2e7cd461c79883e1eb4ccbf758
Shoichi Nakamura <41470674+shoichi1031da@users.noreply.github.com>
2022-07-24 08:16:11 +0900

    add coment

---
+++ app.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+const cors = require("cors");
+
+    app.use(cors({
+        origin: "https://fukuno.jig.jp",
+    }));
+
+const bodyParser = require("body-parser");
+
+    app.use(bodyParser.urlencoded({
+        extended: true
+    }));
+
+    app.use(bodyParser.json()); 
+
+app.post("/",(req,res) => {
+    const token = req.body.token;
+    const msg = req.body.msg;
+        console.log("トークン:" + token);
+        console.log("メッセージ:" + msg);
+    sendLine(token,msg);
+});
+
+const sendLine = (token,msg) => {
+    const BASE_URL = 'https://notify-api.line.me';
+    const PATH =  '/api/notify';
+    const params = new URLSearchParams({
+        message: "\n" + msg,
+    });
+
+    const config = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Authorization': `Bearer ${token}`
+        },
+        body: params.toString()
+    };
+
+    const main = async () => {
+        const res = await fetch(BASE_URL + PATH, config);
+        console.log(res.status);
+    }
+
+    main();
+}
+
+app.listen(PORT,() => {
+    console.log("listening at " + PORT)
+});
