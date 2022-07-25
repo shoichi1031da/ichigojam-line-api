de368ebf43e3d7ebca1f0ea0eafe44a6a29b5194
Shoichi Nakamura <41470674+shoichi1031da@users.noreply.github.com>
2022-07-25 14:22:49 +0900

    ver2.1.1 fix decode for IchigoJam chara

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const cors = require("cors");
 
-const IchigoJamEncoder = require("./IchigoJamEncoder");
+//後日IchigoJam絵文字に対応予定
+    // const IchigoJamEncoder = require("./IchigoJamEncoder");
 
 //IchigoJamWebはPOSTに非対応
     // app.use(cors({
@@ -17,20 +18,40 @@ const bodyParser = require("body-parser");
     app.use(bodyParser.urlencoded({
         extended: true
     }));
+
+const decodeMsg = (msg) => {
+    let decodeLength = 0;
+    let divideMsg = [];
+    let decodedMsg = "";
+    for(let i = 0; i < msg.length; i++){
+        if(msg.substr(i,1) == "%"){
+            divideMsg[decodeLength] = msg.substr(i,6);
+            i += 6;
+        } else {
+            divideMsg[decodeLength] = msg.substr(i,1);
+        }
+        decodeLength ++;
+    }
+    console.log("文字数：" + decodeLength);
+
+    for(const i in divideMsg){
+        decodedMsg += decodeURI(divideMsg[i]);
+    }
+
+    return decodedMsg;
+}
  
 app.post("/",(req,res) => {
     const token = req.body.token;
     const msg = req.body.msg;
     let sendMsg = "";
-    console.log("msg：" + msg);
-    if(msg.substring(0,1) == "%" ){
-        sendMsg = decodeURI(msg);
-        console.log("デコード：" + sendMsg);
-    }else sendMsg = msg;
+        console.log("デコード前" + msg);
+    sendMsg = decodeMsg(msg);
         console.log("トークン:" + token);
         console.log("メッセージ:" + sendMsg);
+
     sendLine(token,sendMsg);
-    console.log("送信完了");
+        console.log("送信完了");
     res.send("sended LINE message\n");
 });
 
