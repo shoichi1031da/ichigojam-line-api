bae67e95cd4daf71a7a06bc398cdc51ca748ddf6
Shoichi Nakamura <41470674+shoichi1031da@users.noreply.github.com>
2022-07-26 23:43:44 +0900

    add comment in app.js

--- app.js
+++ app.js
@@ -16,20 +16,25 @@ const bodyParser = require("body-parser");
         extended: true
     }));
 
+//IchigoJamから送られてきたエンコードされた文字列をデコードする
 const decodeMsg = (msg) => {
-    let decodeLength = 0;
-    let divideMsg = [];
-    let decodedMsg = "";
+    let decodeLength = 0; //デコードする文字列の長さ
+    let divideMsg = []; //デコード前のURIを1文字あたりに分割して格納
+    let decodedMsg = ""; //返り値
+
     for(let i = 0; i < msg.length; i++){
-        let d0 = msg.substr(i+0,1);
-        let d10 = msg.substr(i+10,1);
-        let d13 = msg.substr(i+13,1);
+        let d0 = msg.substr(i+0,1); //デコード前のURIの先頭文字列
+        let d10 = msg.substr(i+10,1); //最終文字列1
+        let d13 = msg.substr(i+13,1); //最終文字列2
+
+        //絵文字によってURIの桁数が違うため#を区切り文字列として使用
         if((d0 == "#" && d10 == "#")){
             divideMsg[decodeLength] = msg.substr(i+1,9);
             i += 10;
         } else if(d0 == "#" && d13 == "#"){
             divideMsg[decodeLength] = msg.substr(i+1,12);
             i += 13;
+        //カタカナの時
         }else if(d0 == "%"){
             divideMsg[decodeLength] = msg.substr(i,9);
             i += 8;
@@ -47,7 +52,8 @@ const decodeMsg = (msg) => {
 
     return decodedMsg;
 }
- 
+
+//IchigoJamからのPOSTされた時の処理
 app.post("/",(req,res) => {
     const token = req.body.token;
     const msg = req.body.msg;
@@ -62,6 +68,7 @@ app.post("/",(req,res) => {
     res.send("sended LINE message\n");
 });
 
+//LINE送信部
 const sendLine = (token,msg) => {
     const BASE_URL = 'https://notify-api.line.me';
     const PATH =  '/api/notify';
